832407b71e0f2236b1bb2274ec58beb75eab7e15
박기성 <bleuflares@gmail.com>
2018-09-28 19:26:01 +0900

    removed unnecessary directory, updated sorting and changed file name

--- common_friend_counter.py
+++ common_friend_counter.py
@@ -2,14 +2,14 @@ import re
 import sys
 from pyspark import SparkConf, SparkContext
 
-BUFVAL = -0xffffffff
+PURGE = -0xffffffff
 
 def parser(ids):    
     friends = re.split(r'[^\w]+', ids)
     if len(friends) > 1:
         return friends[0], list(friends[1:])
 def mark_pairs(items):
-    friends = [((items[0], friend), BUFVAL) for friend in items[1]]
+    friends = [((items[0], friend), PURGE) for friend in items[1]]
     common_friends = []
     for friend1 in items[1]:
         for friend2 in items[1]:
@@ -32,8 +32,8 @@ lines = sc.textFile(sys.argv[1])
 line_pairs = lines.map(lambda ids: parser(ids))
 pairs_map = line_pairs.flatMap(mark_pairs)
 map_counts = pairs_map.reduceByKey(lambda a, b: a + b)
-id_pairs = map_counts.filter(lambda (pair, count): count > 0)
-sorted_pairs = id_pairs.filter(lambda (pair, count): pair[0] < pair[1])
+id_pairs = map_counts.filter(lambda (pair, count): count > 0 and pair[0] < pair[1])
+sorted_pairs = id_pairs.sortBy(lambda item: item[1])
 top_10 = sorted_pairs.take(10)
 write_to_file(top_10, sys.argv[2])
 
