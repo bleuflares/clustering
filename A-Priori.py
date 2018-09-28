5bc1471499a676300d9314738960d97dba9c8727
박기성 <bleuflares@gmail.com>
2018-09-28 22:58:58 +0900

    A-priori algorithm implemented

---
+++ A-Priori.py
@@ -0,0 +1,53 @@
+import sys
+
+input_file = open(sys.argv[1], 'r')
+s = 100 #support value
+session_list = []
+item_dict = {}
+for line in input_file:
+	item_list = line.split()
+	for item in item_list:
+		dict_item = item_dict.get(item)
+		if dict_item == None:
+			item_dict[item] = 1
+		else:
+			item_dict[item] = dict_item + 1
+	session_list.append(item_list)
+
+frequent_items = []
+dict_keys = item_dict.keys()
+for key in dict_keys:
+	if item_dict[key] >= s:
+		frequent_items.append(key)
+
+candidate_pairs = [0] * len(frequent_items) * len(frequent_items)
+for session in session_list:
+	for item1 in session:
+		for item2 in session:
+			if item1 != item2 and item1 in frequent_items and item2 in frequent_items:
+				pair_index = frequent_items.index(item1) * len(frequent_items) + frequent_items.index(item2)
+				if frequent_items.index(item1) < frequent_items.index(item2):
+					candidate_pairs[pair_index] = candidate_pairs[pair_index] + 1
+
+frequent_pairs_count = 0
+frequent_pairs = []
+for i in range(len(candidate_pairs)):
+	if candidate_pairs[i] > s:
+		frequent_pairs_count += 1
+
+top_10_indices = sorted(range(len(candidate_pairs)), key=lambda i: candidate_pairs[i])[-10:]
+
+top_10 = []
+for index in top_10_indices:
+	x = frequent_items[index / len(frequent_items)]
+	y = frequent_items[index % len(frequent_items)]
+	top_10.append(sorted((x, y)))
+top_10.sort(key=lambda tup: tup[0], reverse=True)
+
+output_file = open(sys.argv[2], 'w')
+output_file.write("%d\n" %len(frequent_items))
+output_file.write("%d\n" %frequent_pairs_count)
+for element in top_10:
+	output_file.write("%s\t%s\n" %(element[0], element[1]))
+input_file.close()
+output_file.close()
--- common_friend_counter.py
+++ common_friend_counter.py
@@ -8,6 +8,7 @@ def parser(ids):
     friends = re.split(r'[^\w]+', ids)
     if len(friends) > 1:
         return friends[0], list(friends[1:])
+
 def mark_pairs(items):
     friends = [((items[0], friend), PURGE) for friend in items[1]]
     common_friends = []
@@ -17,24 +18,21 @@ def mark_pairs(items):
                 common_friends.append(((friend1, friend2), 1))
     return friends + common_friends
 
-def pair_to_list(sorted_pairs):
-    return [sorted_pairs[0][0], sorted_pairs[0][1], sorted_pairs[1]]
-
 def write_to_file(content, filename):
     output = open(filename, 'w')
     for item in content:
         output.write("%d\t%d\t%d\n" %(int(item[0][0]), int(item[0][1]), int(item[1])))
     output.close()
 
-conf = SparkConf()
-sc = SparkContext(conf=conf)
-lines = sc.textFile(sys.argv[1])
-line_pairs = lines.map(lambda ids: parser(ids))
-pairs_map = line_pairs.flatMap(mark_pairs)
-map_counts = pairs_map.reduceByKey(lambda a, b: a + b)
-id_pairs = map_counts.filter(lambda (pair, count): count > 0 and pair[0] < pair[1])
-sorted_pairs = id_pairs.sortBy(lambda item: item[1])
-top_10 = sorted_pairs.take(10)
-write_to_file(top_10, sys.argv[2])
+if __name__ == "__main__":
 
-sc.stop()
+    conf = SparkConf()
+    sc = SparkContext(conf=conf)
+    lines = sc.textFile(sys.argv[1])
+    line_pairs = lines.map(lambda ids: parser(ids))
+    pairs_map = line_pairs.flatMap(mark_pairs)
+    map_counts = pairs_map.reduceByKey(lambda a, b: a + b)
+    id_pairs = map_counts.filter(lambda (pair, count): count > 0 and pair[0] < pair[1])
+    top_10 = id_pairs.takeOrdered(10, key=lambda x: -x)
+    write_to_file(top_10, sys.argv[2])
+    sc.stop()
